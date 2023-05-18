8be06778bf1ba220ce7ecf51dc27a2aa0fea9566
Asif419 <asshah419@gmail.com>
2023-05-18 20:56:01 +0600

    banner description

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,10 +1,10 @@
-import { FaSearchengin, FaJenkins, FaBreadSlice, FaHandPeace } from 'react-icons/fa';
+import { FaSearchengin, FaMedapps, FaHandPeace } from 'react-icons/fa';
 
 const Banner = () => {
   return (
     <div className="hero min-h-[500px] bg-base-200 rounded-lg p-2 md:p-5">
       <div className="hero-content grid grid-cols-1 md:grid-cols-2 items-center">
-        <div className="h-[250px] lg:h-[500px] carousel carousel-vertical px-10">
+        <div className="h-[250px] lg:h-[480px] carousel carousel-vertical px-10">
           <div className="carousel-item">
             <img src="https://i.ibb.co/VBFc1vP/Horse.webp" alt="Wolf" className="" />
           </div>
@@ -17,29 +17,26 @@ const Banner = () => {
           {/* Add more carousel items as needed */}
         </div>
         <div className="">
-          <h1 className="text-5xl font-bold font-black">Welcome to ToySafariWorld</h1>
-          <p className="my-5">
+          <h1 className="text-5xl font-bold text-black">Welcome to ToySafariWorld</h1>
+          <p className="my-3 md:my-5">
             Welcome to ToySafariWorld! Discover a captivating collection of animal toys and embark on exciting adventures. Explore our wide range of high-quality toys, featuring wild creatures, domestic pets, and birds. Immerse yourself in imaginative play with realistic and beautifully crafted toys. With our user-friendly website, finding your favorite toys is a breeze. Let your imagination run wild as you explore the world of animal toys at ToySafariWorld. Start your adventure today!
           </p>
-          <div className='text-1xl text-black grid grid-cols-2'>
+          <div className='text-1xl text-black grid grid-cols-2 mb-3 md:mb-5'>
             <span className='flex gap-2 justify-items-center items-center'>
               <FaSearchengin />
-              Founding year: 2023
+              New Arrival: Friday
             </span>
             <span className='flex gap-2 justify-items-center items-center'>
-              <FaJenkins />
-              Chefs: 6
-            </span>
-            <span className='flex gap-2 justify-items-center items-center'>
-              <FaBreadSlice />
-              Recipes: 170+
+              <FaMedapps />
+              Discount: 5%
             </span>
             <span className='flex gap-2 justify-items-center items-center'>
               <FaHandPeace />
-              Review: 29700+
+              Review: 30,000+
             </span>
           </div>
-          <button className="btn bg-black text-white">Buy Now!</button>
+          <button className="btn bg-black text-white mr-2">Buy Now!</button>
+          <button className="btn btn-primary">Get Coupon</button>
         </div>
       </div>
     </div>
