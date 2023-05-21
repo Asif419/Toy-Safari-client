2b1d618d11bc71a26b9bba11628b04fc57160619
Asif419 <asshah419@gmail.com>
2023-05-21 17:40:52 +0600

    blogs answer added

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -14,20 +14,20 @@ const Blogs = () => {
       <div className='p-5 md:p-10 py-10 md:py-16'>
         <div className='grid grid-cols-1 gap-8 drop-shadow-md'>
           <div className='border p-3 md:p-5 rounded-lg bg-gray-100' data-aos="fade-right" data-aos-duration="600">
-            <p className='font-bold tracking-wider'>Differences between uncontrolled and controlled components?</p>
-            <p className='mt-3'><span className='font-semibold'>Answer:</span> Which component manages its own state internally that is known as uncontrolled component. On the other hand controlled component is controlled by the parent component. In uncontrolled component while user interacts then its internal state updated directly. But controlled component controlled by calling function and using props.</p>
+            <p className='font-bold tracking-wider mb-4'>What is an access token and refresh token? How do they work and where should we store them on the client-side?</p>
+            <p className='mt-3'><span className='font-semibold'>Answer:</span> Similar to a unique key, an access token grants you access to restricted resources. A refresh token will replace your key after it has expired. To ensure that you remain signed in and are able to perform the tasks that are permitted of you, these tokens are kept secure on your device, either in a cookie or storage.</p>
           </div>
           <div className='border p-3 md:p-5 rounded-lg bg-gray-100' data-aos="fade-left" data-aos-duration="600">
-            <p className='font-bold tracking-wider'>How to validate React props using PropTypes?</p>
-            <p className='mt-3'><span className='font-semibold'>Answer:</span>PropTypes is a package. Which usually validate react component's props. Developer can use it by installing via npm or yearn. If a component pass invalid type of props then PropTypes will throw and error and also console an alert.</p>
+            <p className='font-bold tracking-wider mb-4'>Compare SQL and NoSQL databases</p>
+            <p className='mt-3'><span className='font-semibold'>Answer:</span>An ordered filing cabinet with labels on each file is how SQL databases operate. To search for and manage data, they employ the SQL language. NoSQL databases are more akin to a cluttered drawer where anything can be thrown in. They are versatile and can handle many data formats. NoSQL is better for handling large amounts of disorganized data, whereas SQL is better for complicated queries and transactions.</p>
           </div>
           <div className='border p-3 md:p-5 rounded-lg bg-gray-100' data-aos="fade-right" data-aos-duration="800">
-            <p className='font-bold tracking-wider'>Differences between nodejs and express js.</p>
-            <p className=''><span className='font-semibold'>Answer:  </span>Both tools used for web development though both of them have different purpose to complete. Node.js is actually is a runtime environment tools that run JavaScript code outside a browser. It makes platform for making web servers and also network applications. On top of that, Express.js is a web application framework. Main task of it is work as middleware for handling HTTP requests and responses. Also it control routing and template rendering.</p>
+            <p className='font-bold tracking-wider mb-4'>What is express js? What is Nest JS?</p>
+            <p className=''><span className='font-semibold'>Answer:  </span>A well-liked web application framework for Node.js is called Express.js. By offering a collection of tools and utilities, it makes the process of creating web servers and APIs simpler. On top of Express.js, NestJS is a TypeScript-based framework. To improve code structure and scalability, it includes further capabilities like dependency injection and architectural patterns.</p>
           </div>
           <div className='border p-3 md:p-5 rounded-lg bg-gray-100' data-aos="fade-left" data-aos-duration="1000">
-            <p className='font-bold tracking-wider'>What is a custom hook, and why will you create a custom hook?</p>
-            <p className=''><span className='font-semibold'>Answer:</span>Mainly, custom hook is a JS function that uses React Hooks. It returns logic which can be shared through multiple components. For multiple purpose we can use custom hook. For example, 1. Not to repeat same code, 2. Can be used on encapsulating stateful logic.</p>
+            <p className='font-bold tracking-wider mb-4'>What is MongoDB aggregate and how does it work?</p>
+            <p className=''><span className='font-semibold'>Answer:</span>Aggregate in MongoDB is a strong function that lets you do complex operations on your data. It works by combining multiple stages like grouping, filtering, sorting, and calculating to process and transform your data. It's like a pipeline where data flows through different stages to produce the desired results, enabling you to perform complex queries and analysis on your MongoDB data.</p>
           </div>
         </div>
       </div>
